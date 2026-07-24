4cff2ce73b8b50d51bbdddb7ad918c43d57bcdc7
limuyang <limuyang2@hotmail.com>
2026-07-24 16:01:42 +0800

    chore(build): 为PDF查看器模块添加JavaScript目标支持
    - 在构建配置中启用js()目标
    - 为多平台项目添加JavaScript编译支持
    - 允许PDF查看器在Web环境中运行

--- pdf-viewer/build.gradle.kts
+++ pdf-viewer/build.gradle.kts
@@ -28,6 +28,8 @@ kotlin {
         }
     }
 
+    js()
+
     // For iOS targets, this is also where you should
     // configure native binary output. For more information, see:
     // https://kotlinlang.org/docs/multiplatform-build-native-binaries.html#build-xcframeworks
